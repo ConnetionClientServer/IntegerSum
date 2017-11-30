fd9d6ab321917c813e7bdf5767feec67eefae094
Ivyx <ivana.pietrabissa@outlook.com>
2017-11-30 15:44:30 +0100

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,12 +1,18 @@
 import java.io.IOException;
 import java.net.Socket;
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+
 
 public class Client {
 	public static void main(String[] args) {
+		//byte[] buffer = new byte[500];
 		try {
 			System.out.println("Adesso provo a connettermi a 127.0.0.1");
 			Socket client = new Socket("127.0.0.1" , 9000);
 			System.out.println("Connesso");
+			
+			//BufferedReader byServer = new BufferedReader (new InputStreamReader(client.getInputStream()));
 		}
 		catch(IOException e){
 			System.out.println(e);
