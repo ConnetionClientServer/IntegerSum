9e5d3b4e897b70d887011bda4f21bb3b71bb6323
Ivyx <ivana.pietrabissa@outlook.com>
2017-12-01 11:05:30 +0100

    Update Server.java

--- Server.java
+++ Server.java
@@ -5,7 +5,10 @@ import java.net.SocketTimeoutException;
 
 public class Server {
 	public static void main(String[] args) {
-		//String operator, number, number2;
+		String operator; 
+       	int number, number2;
+        int total;
+		
 	    try {
 	    	ServerSocket service = new ServerSocket(9000);
 	    	service.setSoTimeout(10000);
@@ -14,22 +17,27 @@ public class Server {
 	    	System.out.println("Un client si è connesso!");
 		    
 		    
-		BufferedReader byClient = new BufferedReader (new InputStreamReader(socket.getInputStream()));
-            	BufferedWriter toClient = new BufferedWriter (new OutputStreamWriter(socket.getOutputStream()));
-            	toClient.write("Dammi i numeri da sommare!! Adesso!");
-            	toClient.flush();
+			BufferedReader byClient = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+            BufferedWriter toClient = new BufferedWriter (new OutputStreamWriter(socket.getOutputStream()));
+            toClient.write("Dammi i numeri da sommare!! Adesso!");
+            toClient.flush();
 		    
-		operator = byClient.readLine();
-            	System.out.println("E' stato inserito l'operatore " + operator);
-		    
-/******  Arrivato qui il server mi stampa tutto il flusso client all'interno di operator, ad esempio "+25"  ******/
+			operator = byClient.readLine();
+            System.out.println("E' stato inserito l'operatore: " + operator);
+			
+			number = Integer.parseInt(byClient.readLine());
+            System.out.println("E' stato inserito il 1° numero: " + number);
             
-            	//number = byClient.readLine();
-            	//System.out.println("E' stato inserito il 1 numero " + number);
+            number2 = Integer.parseInt(byClient.readLine());
+            System.out.println("E' stato inserito il 2° numero: " + number2);
             
-            	//number2 = byClient.readLine();
-            	//System.out.println("E' stato inserito il 2 numero " + number2);
-		    
+            if(!operator.equals("-") )  {
+                total = number+number2;
+            }else
+                total = number-number2;
+            
+            System.out.println("Il tuo risultato è: " + number + operator + number2 +"= " + total);
+			
 	    	socket.close();
 	    	service.close();
 	    }
